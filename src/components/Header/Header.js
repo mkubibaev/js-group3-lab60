a7128c822a2212715e6d37f0643a376e03212a01
mkubibaev <mkubibaev@gmail.com>
2019-01-17 19:58:55 +0600

    added main container and header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h4>Group chat</h4>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/containers/MainContainer/MainContainer.js
@@ -0,0 +1,17 @@
+import React, {Component, Fragment} from 'react';
+import Header from "../../components/Header/Header";
+
+class MainContainer extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Header/>
+                <div className="container">
+
+                </div>
+            </Fragment>
+        );
+    }
+}
+
+export default MainContainer;
