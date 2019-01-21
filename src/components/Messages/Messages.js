8ebbef7cf482ebe8d3e1b62413ac347a327e3f92
mkubibaev <mkubibaev@gmail.com>
2019-01-21 17:27:54 +0600

    changed msg date

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -6,14 +6,19 @@ import './Messages.css';
 const Messages = props => {
     return (
         <div className="messages">
-            {props.messages.map(message => (
-                <Message
-                    key={message._id}
-                    author={message.author}
-                    message={message.message}
-                    datetime={message.datetime}
-                />
-            ))}
+            {props.messages.map(message => {
+                const messageDate = new Date(message.datetime);
+                const formattedDate = messageDate.toLocaleString('ru-Ru');
+
+                return (
+                    <Message
+                        key={message._id}
+                        author={message.author}
+                        message={message.message}
+                        datetime={formattedDate}
+                    />
+                )
+            })}
         </div>
     );
 };
